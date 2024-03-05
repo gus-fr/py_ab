1ba36ca9588e310f907b35880477ca983c3f225f
Gus <g.frigo@stuart.com>
2024-03-05 22:59:15 +0100

    added abtest on parser

---
+++ src/pyab_experiment/parser.py
@@ -0,0 +1,9 @@
+from pyab_experiment.data_structures.syntax_tree import ExperimentAST
+from pyab_experiment.language.grammar import ExperimentParser
+from pyab_experiment.language.lexer import ExperimentLexer
+
+
+def parse(text: str) -> ExperimentAST:
+    lexer = ExperimentLexer()
+    parser = ExperimentParser()
+    return parser.parse(lexer.tokenize(text))
