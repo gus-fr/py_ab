48c18a26199b90bc5e7ad27c2bc6038e1b065180
Gus <g.frigo@stuart.com>
2024-03-05 22:59:51 +0100

    docstring

--- src/pyab_experiment/parser.py
+++ src/pyab_experiment/parser.py
@@ -1,3 +1,4 @@
+"""parsing module"""
 from pyab_experiment.data_structures.syntax_tree import ExperimentAST
 from pyab_experiment.language.grammar import ExperimentParser
 from pyab_experiment.language.lexer import ExperimentLexer
