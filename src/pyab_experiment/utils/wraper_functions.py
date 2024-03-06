5de966b534b74c1c7ba00553eaad3bbc2a871f5f
Gus <g.frigo@stuart.com>
2024-03-06 22:02:17 +0100

    refactor folder

--- src/pyab_experiment/utils/wraper_functions.py
+++ src/pyab_experiment/utils/wraper_functions.py
@@ -2,7 +2,7 @@
 from pyab_experiment.data_structures.syntax_tree import ExperimentAST
 from pyab_experiment.language.grammar import ExperimentParser
 from pyab_experiment.language.lexer import ExperimentLexer
-from pyab_experiment.codegen.python_generator import PythonCodeGen
+from pyab_experiment.codegen.python.python_generator import PythonCodeGen
 
 
 def parse(text: str) -> ExperimentAST:
