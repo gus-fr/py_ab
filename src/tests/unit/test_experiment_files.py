25b629c841581ccb74ffb4c5cc32d34b5a969955
Gus <g.frigo@stuart.com>
2024-03-07 14:36:15 +0100

    refactored repetitive unit tests

---
+++ src/tests/unit/test_experiment_files.py
@@ -0,0 +1,46 @@
+"""test the parsing, lexing, and codegen"""
+from pathlib import Path
+
+import pytest
+
+from pyab_experiment.utils.wraper_functions import generate_code,parse_source
+from pyab_experiment.language.lexer import ExperimentLexer
+
+files_to_test = [
+    "basic_experiment.pyab",
+    "comments.pyab",
+    "full_grammar.pyab",
+    "salt.pyab",
+    "splitters.pyab",
+    "conditional.pyab",
+]
+
+
+@pytest.mark.parametrize("file_name", files_to_test)
+def test_generator(file_name):
+    """all files should generate some code without problems"""
+    with open(
+        f"{Path(__file__).absolute().parent}/test_programs/{file_name}", "r"
+    ) as fp:
+        python_code = generate_code(fp.read())
+        assert python_code is not None
+
+
+@pytest.mark.parametrize("file_name", files_to_test)
+def test_parsable_programs(file_name):
+    """all files should parse withour syntaxerrors"""
+    with open(
+        f"{Path(__file__).absolute().parent}/test_programs/{file_name}", "r"
+    ) as fp:
+        ast = parse_source(fp.read())
+        assert ast is not None
+
+
+@pytest.mark.parametrize("file_name", files_to_test)
+def test_lexer(file_name):
+    """all files should lex without token errors (strange chars) errors"""
+    with open(
+        f"{Path(__file__).absolute().parent}/test_programs/{file_name}", "r"
+    ) as fp:
+        lexer = ExperimentLexer()
+        assert len([token for token in lexer.tokenize(fp.read())]) > 0
