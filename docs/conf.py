78642d7794c4c1443d54d41d769af36959076a9f
Gus <g.frigo@stuart.com>
2024-03-13 16:00:44 +0100

    refactor docs

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 
 project = "py-ab-experiment"
 copyright = "2024, gus-fr"
-author = "gus-fr"
+author = "[gus-fr](https://github.com/gus-fr)"
 release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
@@ -23,7 +23,20 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+# html_static_path = ["_static"]
+
+# -- AutoApi settings ---------------------------------------------------------
 
 autoapi_dirs = ["../src"]
+
+
+def skip_sly(app, what, name, obj, skip, options):
+    if name.startswith("pyab_experiment.sly") and what != "package":
+        #    if what == "class" and "util" in name:
+        skip = True
+    return skip
+
+
+def setup(sphinx):
+    sphinx.connect("autoapi-skip-member", skip_sly)
--- docs/index.rst
+++ docs/index.rst
@@ -3,11 +3,11 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to py-ab-experiment's documentation!
+py-ab-experiment docs
 ============================================
 
 .. toctree::
-   :maxdepth: 23
+   :maxdepth: 2
    :caption: Contents:
 
    readme
