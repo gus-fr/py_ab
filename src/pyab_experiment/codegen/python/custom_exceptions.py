8502cd81dc553c65ea3a64ac1cbc9cdf766332e1
Gus <g.frigo@stuart.com>
2024-12-12 16:23:30 +0100

    add custom exception

---
+++ src/pyab_experiment/codegen/python/custom_exceptions.py
@@ -0,0 +1,8 @@
+class ExperimentConditionalFailedError(Exception):
+    """Raised whenever the experiment conditional evaluation
+    fails. This can occurs whenever defaults are not properly
+    defined on an experiment conditional
+    """
+
+    def __init__(self, message="Experiment conditional evaluation failed"):
+        super().__init__(message)
